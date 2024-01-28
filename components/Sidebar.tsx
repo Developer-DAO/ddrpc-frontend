40297173d8c4d44947c1dd2b377993dfcb0f061a
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2024-01-28 11:28:55 -0500

    feat: basic components for navbar and sidebar set up.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+
 export default function Sidebar() {
-    return (
-        <div>Sidebar</div>
-    );
+  return (
+    // SIDEBAR CONTAINER
+    <aside className="fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-lg shaodw-gray-900/20 transition duration-300 ease-in-out w-[20rem]">
+      {/* APP LOGO */}
+
+      <div className="flex relative items-center p-4 min-h min-h-fit py-5 px-3.5">
+        <Image alt="" src="/D_D-logo.png" width={500} height={200}></Image>
+      </div>
+
+      {/* APP TITLE */}
+
+      <div>
+        <h3 className="pl-2 font-bold text-2xl text-[#e6e9ee] min-w-max">
+          D_D RPC Dashboard
+        </h3>
+      </div>
+
+      {/* NAVIGATION */}
+
+      <nav></nav>
+    </aside>
+  );
 }
