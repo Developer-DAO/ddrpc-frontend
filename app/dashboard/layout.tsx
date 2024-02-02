f2652b3e92b7efa7c601173182347e1add54849b
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2024-02-02 16:01:40 -0500

    Created homepage layout and migrated all dashboard assets to /dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Karla } from "next/font/google";
+import "../globals.css";
+import Sidebar from "@/app/dashboard/_components/Sidebar";
+import Navbar from "@/app/dashboard/_components/Navbar";
+import PageWrapper from "@/app/dashboard/_components/PageWrapper";
+
+const karla = Karla({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-karla",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={karla.className}>
+        <div className="flex min-h-screen">
+          <Sidebar />
+
+          <Navbar />
+          <PageWrapper children={children}></PageWrapper>
+        </div>
+      </body>
+    </html>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import React from "react";
+
+export default function HomePage() {
   return (
     <>
-      <h2>D_D RPC Dashboard</h2>
+      <div>D_D RPC Home Page</div>
+      <div>
+        <button className="rounded-md p-3 bg-gray-800 text-white m-3">
+          <a href="/dashboard">Go to Dashboard</a>
+        </button>
+      </div>
     </>
   );
 }
