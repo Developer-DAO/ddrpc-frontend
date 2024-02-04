cd1faa7763089cf71cd53ec66820c472437a57e8
Adam Sobotka <vorcigernix@gmail.com>
2024-02-04 16:22:49 +0100

    a design idea for homepage

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,19 +1,41 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { Icons } from "./ui/icons"
 
 export function Navigation() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        Main Navigation
+    <header className="sticky top-0 z-50 w-full  bg-zinc-900/95 backdrop-blur supports-[backdrop-filter]:bg-zinc-900/60">
+      <div className="container flex py-4 max-w-screen-2xl items-center">
+        <div className="mr-4 hidden md:flex">
+          <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Icons.logo className="h-12 w-12" />
+            <span className="hidden font-bold sm:inline-block">
+              RPC
+            </span>
+          </Link>
+          <nav className="flex items-center gap-6 text-lg font-semibold">
+            <Link
+              href="/about"
+              className=
+              " tracking-wide text-neutral-500 p-0 hover:text-white transition-colors"
+            >
+              About
+            </Link>
+            <Link
+              href="/contact"
+              className=
+              " tracking-wide text-neutral-500 p-0 hover:text-white transition-colors"
+            >
+              Contact
+            </Link>
+          </nav>
+        </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-
-          </div>
           <nav className="flex items-center">
-            <Button asChild>
-              <Link href="/dashboard" className="mx-2"> Application</Link>
-            </Button>
+
+            <Link href="/dashboard" className="flex w-fit items-center justify-center gap-1 rounded-full border-2 transition-all h-12 px-6 text-lg text-black border-white bg-white font-paragraph font-semibold tracking-wider hover:drop-shadow-[10px_0_20px_rgba(254,254,254,0.472)]" > Application</Link>
+
           </nav>
         </div>
       </div>
