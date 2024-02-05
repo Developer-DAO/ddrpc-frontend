1f8d2d624a45d5d47145472bb5ca29b9b6e33fcf
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2024-02-05 07:46:56 -0500

    Switched from using classnames to cn

--- app/dashboard/_components/PageWrapper.tsx
+++ app/dashboard/_components/PageWrapper.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { ReactNode } from "react";
-import classNames from "classnames";
+import { cn } from "@/lib/utils";
 import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
 
 export default function PageWrapper({ children }: { children: ReactNode }) {
   const { toggleCollapse } = useSidebarToggle();
-  const pageStyle = classNames("bg-slate-50 flex-grow text-black p-2 mt-16", {
+  const pageStyle = cn("bg-slate-50 flex-grow text-black p-2 mt-16", {
     ["sm:pl-[5.6rem]"]: toggleCollapse,
     ["sm:pl-[20rem]"]: !toggleCollapse,
   });
