c757ca6cc8f4702dd58a7bf81acc542b370d28d1
Adam Sobotka <vorcigernix@gmail.com>
2024-02-07 10:51:27 +0100

    fixing the test for CI

--- tests/test-nav.spec.ts
+++ tests/test-nav.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
  
 test('should navigate across pages', async ({ page }) => {
 
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   await page.getByTestId('nav-home').click();
   await expect(page.getByTestId('mainhero')).toBeVisible();
