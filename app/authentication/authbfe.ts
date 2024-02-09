89ce37fc11c520fc6f1472e0a61257cd5a8b91fc
Adam Sobotka <vorcigernix@gmail.com>
2024-02-09 18:34:16 +0100

    current registration state

--- app/authentication/authbfe.ts
+++ app/authentication/authbfe.ts
@@ -1,21 +1,23 @@
 "use server"
 export async function registerUser(formData: FormData) {
 
-    const rawFormData = {
-        wallet: formData.get('wallet'),
-        email: formData.get('email'),
-        password: formData.get('password'),
-    }
+  const rawFormData = {
+    email: formData.get('email'),
+    wallet: formData.get('wallet'),
+    password: formData.get('password'),
+  }
 
-    console.log(rawFormData);
+  console.log(rawFormData);
 
-    const response = await fetch('https://api.example.com/user/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(rawFormData),
-    })
+  const response = await fetch('http://127.0.0.1:2345/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(rawFormData),
+  })
 
-    return { message: `Registered user  ${rawFormData.email}` };
+  console.info(await response.text());
+
+  return { message: `Registered user  ${rawFormData.email}` };
 }
