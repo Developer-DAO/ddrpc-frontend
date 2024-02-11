3d3349be34418c9e47b07ae2d00e1da8db197312
Adam Sobotka <vorcigernix@gmail.com>
2024-02-11 11:33:10 +0100

    registration flow works

--- app/authentication/authbfe.ts
+++ app/authentication/authbfe.ts
@@ -1,5 +1,5 @@
 "use server"
-export async function registerUser(formData: FormData) {
+export async function registerUser(prevState: any, formData: FormData) {
 
   const rawFormData = {
     email: formData.get('email'),
@@ -7,17 +7,21 @@ export async function registerUser(formData: FormData) {
     password: formData.get('password'),
   }
 
-  console.log(rawFormData);
+  //console.log(rawFormData);
 
-  const response = await fetch('http://127.0.0.1:2345/api/register', {
+  const response = await fetch('http://0.0.0.0:3000/api/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(rawFormData),
   })
+  const result = await response;
+  if (result.status === 200) {
+    return { message: `Registered user  ${rawFormData.email}`, success: true };
+  }
+  else {
 
-  console.info(await response.text());
-
-  return { message: `Registered user  ${rawFormData.email}` };
+    return { message: `Error: ${result.statusText}`, success: false };
+  }
 }
