9ef79705ff5ac76665467658e6166d840ac50a0e
kempsterrrr <kempsterwilliam@gmail.com>
2024-04-03 16:16:11 -0600

    adding blockchain logo's

--- app/page.tsx
+++ app/page.tsx
@@ -68,35 +68,35 @@ export default function Example() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+            src="ethereum-logo.svg"
             alt="Transistor"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 p-4"
+            src="solana-wordmark-white.svg"
             alt="Reform"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 p-2"
+            src="polygon-white-logo.svg"
             alt="Tuple"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 p-3"
+            src="scroll-logo.svg"
             alt="SavvyCal"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg"
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1 p-2"
+            src="sui-logo-white.svg"
             alt="Statamic"
             width={158}
             height={48}
