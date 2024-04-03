f725a3b58653f40a941ba1a1b511868a44e2bffd
kempsterrrr <kempsterwilliam@gmail.com>
2024-04-03 14:21:30 -0600

    removing navigation and standardising messaging inline with PR launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import { Metadata } from "next"
-import { headers } from 'next/headers'
+import { Metadata } from "next";
+import { headers } from "next/headers";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme";
-import { Navigation } from "@/components/navigation"
+import { Navigation } from "@/components/navigation";
 import { cn } from "@/lib/utils";
 import { Footer } from "@/components/footer";
-import { config } from '../wagmi.config'
-import { cookieToInitialState } from 'wagmi'
-import { Providers } from './providers'
+import { config } from "../wagmi.config";
+import { cookieToInitialState } from "wagmi";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,75 +17,72 @@ export const metadata: Metadata = {
     default: "D_D RPC",
     template: `%s - "D_D RPC"`,
   },
-  //chore add real url
-  metadataBase: new URL("https://ddrpc.developerdao.com"),
-  description: "D_D RPC is a gatweay to Web3 world.",
-  keywords: [
-    "Developer DAO",
-    "Web3",
-    "RPC",
-    "Gateway",
-    "Decentralized",
-  ],
+  metadataBase: new URL("https://rpc.developerdao.com"),
+  description:
+    "High-performance and censorship-resistant access to Blockchain data from the first DAO-owned RPC Gateway.",
+  keywords: ["Developer DAO", "Web3", "RPC", "Gateway", "Decentralized"],
   authors: [
     {
       //chore add other authors
-      name: "@PSkinnerTech, @vorcigernix",
+      name: "@PSkinnerTech, @vorcigernix, @kempsterrrr",
     },
   ],
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://ddrpc.developerdao.com",
-    title: "D_D RPC",
-    description: "D_D RPC is a gatweay to Web3 world.",
-    siteName: "D_D RPC",
+    url: "https://rpc.developerdao.com",
+    title: "Developer DAO RPC | Access Blockchain Data, Fund Public Goods",
+    description:
+      "High-performance and censorship-resistant access to Blockchain data from the first DAO-owned RPC Gateway.",
+    siteName: "Developer DAO RPC",
     images: [
       {
-        url: "https://ddrpc.developerdao.com/og-image.png",
+        url: "https://rpc.developerdao.com/og-image.png",
         width: 1200,
         height: 630,
-        alt: "D_D RPC",
+        alt: "Developer DAO RPC Gateway. Access Blockchain Data, Fund Public Goods",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "D_D RPC",
-    description: "D_D RPC is a gatweay to Web3 world.",
-    images: "https://ddrpc.developerdao.com/og-image.png",
+    title: "Developer DAO RPC | Access Blockchain Data, Fund Public Goods",
+    description:
+      "High-performance and censorship-resistant access to Blockchain data from the first DAO-owned RPC Gateway.",
+    images: "https://rpc.developerdao.com/og-image.png",
   },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-}
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const initialState = cookieToInitialState(config, headers().get('cookie'));
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
 
       <body
-        className={cn(
-          "min-h-screen bg-zinc-900 antialiased",
-          inter.className
-        )}
-      >   <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
+        className={cn("min-h-screen bg-zinc-900 antialiased", inter.className)}
       >
-          <Providers initialState={initialState}><div>{children}</div></Providers>
-        </ThemeProvider></body>
-
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers initialState={initialState}>
+            <div>{children}</div>
+          </Providers>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function HomePage() {
     <div className="relative flex min-h-screen flex-col ">
       <Navigation />
       <main className="flex-1">
-
         <section>
           <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
             <div className="flex flex-wrap items-center mx-auto max-w-7xl">
@@ -18,32 +17,47 @@ export default function HomePage() {
                     <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-500 rounded-full opacity-70  filter blur-xl animate-blob animation-delay-2000"></div>
                     <div className="absolute -bottom-8 left-28 w-72 h-72 bg-blue-500 rounded-full opacity-70  filter blur-xl animate-blob animation-delay-4000"></div>
                     <div className="relative">
-                      <img className="object-cover object-center mx-auto mix-blend-color-dodge" alt="hero" src="eth-diamond-purple.svg" />
+                      <img
+                        className="object-cover object-center mx-auto mix-blend-color-dodge"
+                        alt="hero"
+                        src="eth-diamond-purple.svg"
+                      />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0" data-testid="mainhero">
-                <span className="mb-8 text-xs font-bold tracking-widest text-neutral-600 uppercase"> remote procedure call gateway by D_D </span>
-                <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-100 md:text-7xl lg:text-5xl">Your gateway to the decentralized world.</h1>
-                <p className="mb-8 text-base leading-relaxed text-left text-neutral-500">Some other claim why we are building this,  other claim why we are building this. Perfecto! </p>
-                <p className="mb-2 text-base leading-relaxed text-left text-neutral-500">Don't miss the opportunity, register now!</p>
+              <div
+                className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0"
+                data-testid="mainhero"
+              >
+                <span className="mb-8 text-xs font-bold tracking-widest text-neutral-600 uppercase">
+                  An RPC Gateway By Developer DAO
+                </span>
+                <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-100 md:text-7xl lg:text-5xl">
+                  Access Blockchain Data. Fund Public Goods.
+                </h1>
+                <p className="mb-8 text-base leading-relaxed text-left text-neutral-500">
+                  High-performance and censorship-resistant access to Blockchain
+                  data from the first DAO-owned RPC Gateway.
+                </p>
                 <div className="flex-col mt-0 lg:mt-6 max-w-7xl sm:flex">
-                  <form method="post" id="ddrpc-form" name="ddrpc-form" target="_blank" className="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-neutral-50 rounded-xl sm:max-w-lg sm:flex">
-                    <div className="flex-1 min-w-0 ddrpc-form-group">
-                      <label htmlFor="member_email" className="sr-only">Email address</label>
-                      <input id="member_email" type="email" className="block w-full px-5 py-3 text-base placeholder-neutral-300 dark:placeholder-neutral-500 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md text-neutral-600 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-300" placeholder="Enter your email  " />
-                    </div>
-                    <div className="mt-4 sm:mt-0 sm:ml-3 ddrpc-form-actions">
-                      <button type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" className="block w-full px-5 py-3 text-base font-medium text-white bg-neutral-600 border border-transparent rounded-full shadow hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-300 sm:px-10">Notify me</button>
-                    </div>
-                  </form>
+                  <CaptureForm />
                   <div className="sm:max-w-lg sm:flex">
                     <p className="mt-3 text-xs text-neutral-500">
                       By subscribing, you agree with Developer DAO RPC gateway
-                      <a target="_blank" href="https://developerdao.com/terms"> Terms of Service </a>
+                      <a target="_blank" href="https://developerdao.com/terms">
+                        {" "}
+                        Terms of Service{" "}
+                      </a>
                       and
-                      <a target="_blank" href="https://developerdao.com/privacy"> Privacy Policy</a>.
+                      <a
+                        target="_blank"
+                        href="https://developerdao.com/privacy"
+                      >
+                        {" "}
+                        Privacy Policy
+                      </a>
+                      .
                     </p>
                   </div>
                 </div>
@@ -51,9 +65,43 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-
       </main>
       <Footer />
     </div>
   );
 }
+
+const CaptureForm = () => {
+  return (
+    <form
+      method="post"
+      id="ddrpc-form"
+      name="ddrpc-form"
+      target="_blank"
+      className="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-neutral-50 rounded-xl sm:max-w-lg sm:flex"
+    >
+      <div className="flex-1 min-w-0 ddrpc-form-group">
+        <label htmlFor="member_email" className="sr-only">
+          Email address
+        </label>
+        <input
+          id="member_email"
+          type="email"
+          className="block w-full px-5 py-3 text-base placeholder-neutral-300 dark:placeholder-neutral-500 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md text-neutral-600 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-300"
+          placeholder="Enter your email  "
+        />
+      </div>
+      <div className="mt-4 sm:mt-0 sm:ml-3 ddrpc-form-actions">
+        <button
+          type="submit"
+          value="Subscribe"
+          name="member[subscribe]"
+          id="member_submit"
+          className="block w-full px-5 py-3 text-base font-medium text-white bg-neutral-600 border border-transparent rounded-full shadow hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-300 sm:px-10"
+        >
+          Notify me
+        </button>
+      </div>
+    </form>
+  );
+};
