3e3ba162dbc82a6208d47f0682be8d726478d064
kempsterrrr <kempsterwilliam@gmail.com>
2024-04-08 18:31:10 -0600

    adding gs

--- app/page.tsx
+++ app/page.tsx
@@ -165,6 +165,33 @@ export default function Example() {
           />
         </div>
       </div>
+      <footer>
+        <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            {/* {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))} */}
+            <a
+              href="https://www.developerdao.com/privacy"
+              className="text-center text-xs leading-5 text-gray-500"
+            >
+              Privacy Policy
+            </a>
+          </div>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-xs leading-5 text-gray-500">
+              &copy; 2024 The Developer DAO Foundation. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
