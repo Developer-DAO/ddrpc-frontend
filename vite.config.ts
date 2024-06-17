f04760e07b6489e5fde7a8d785a222d913b10631
Saúl García Céspedes <saul@dixwe.com>
2024-06-17 11:42:03 +0200

    chore: update deps and fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	build: {
+    rollupOptions: {
+      external: [
+        'http', 'https', 'zlib', 'stream', 'events', 'path', 'fs', 'util', 'url', 'tty', 'os'
+      ]
+    }
+  }
 });
